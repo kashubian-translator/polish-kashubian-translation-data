9c18a23d17091e4d3071bfd610260e4acef564c6
Karakean <karakean@users.noreply.github.com>
2024-05-30 18:35:24 +0200

    finished first scraper, data added to the dataset

--- scrapers/kaszebe.py
+++ scrapers/kaszebe.py
@@ -1,4 +1,6 @@
 import string
+from time import sleep
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -16,9 +18,38 @@ def remove_last_letter(data):
     data['q'] = data['q'][:len(data['q']) - 1]
 
 
+def send_request(url, method, data, attempt):
+    try:
+        if method == 'post':
+            response = requests.post(url, data=data)
+        elif method == 'get':
+            response = requests.get(url)
+        else:
+            print("Method not implemented")
+            return None
+        if 200 <= response.status_code < 300:
+            return response
+        print(f"Attempt {attempt} failed: Unsuccessful status code {response.status_code}\n"
+              f"Response JSON body: {response.json()}\n")
+    except requests.RequestException as e:
+        print(f"Attempt {attempt} failed: Request failed: {e}\n")
+    return None
+
+
+def send_request_with_retries(url, method='get', data=None, retries=6, delay=10):
+    for idx in range(retries):
+        response = send_request(url, method, data, idx+1)
+        if response is not None:
+            return response
+        print(f"Retrying in {delay} seconds...\n")
+        sleep(delay)
+    print("Maximum number of retries exceeded\n")
+    return None
+
+
 def fetch_translations(word):
     word_url = f"{word_url_prefix}{word}"
-    response = requests.get(word_url)
+    response = send_request_with_retries(word_url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
         translations = soup.find_all('ul', class_='translations-list')
@@ -30,10 +61,12 @@ def fetch_translations(word):
         return ["ERROR"]
 
 
-def fetch_and_save_phrases_with_translations(request_data, pl_file, csb_file):
+def fetch_and_save_phrases_with_translations(request_data, pl_file, csb_file, start_letter='a'):
     for letter in polish_alphabet:
+        if polish_alphabet.index(letter) < polish_alphabet.index(start_letter):
+            continue
         add_letter(request_data, letter)
-        words_response = requests.post(suggestions_url, data=request_data)
+        words_response = send_request_with_retries(suggestions_url, 'post', request_data)
         if len(words_response.json()) < 10:
             for word in words_response.json():
                 translations = fetch_translations(word['polish'])
